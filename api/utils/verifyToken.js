cce9b6200de4fd14cdc989c622a2d9901c44a91a
zunayed-arp <zunayed.arp@gmail.com>
2022-09-01 19:07:07 +0600

    refactored admin verification

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -27,7 +27,7 @@ export const verifyUser = (req, res, next) => {
 
 
 export const verifyAdmin = (req,res,next) =>{
-  verifyToken(req,res,()=>{
+  verifyToken(req,res,next,()=>{
     if(req.user.isAdmin){
       next();
     }else{
