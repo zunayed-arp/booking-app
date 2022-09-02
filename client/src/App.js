e6da894982d1d6f2f6c9af2d857285a1fb8c4396
zunayed-arp <zunayed.arp@gmail.com>
2022-09-02 11:39:54 +0600

    chore: navbar design completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 function App() {
   return (
-    <div >
-     
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      {/* <Routes path="/hotels" element={<List/>}/> */}
+      {/* <Routes path="/hotels/:id" element={<Hotel/>}/> */}
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
 
